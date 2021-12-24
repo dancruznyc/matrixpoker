b188da62320ce53ad411b7c2ef60a616f8e6ff5e
dancruznyc <dancruznyc@gmail.com>
2021-12-24 11:07:40 -0500

    score board

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const grid = document.querySelector(".game-grid");
+const marqueeMessage = document.querySelector(".marquee-content");
+const gameScoreDisplay = document.querySelector(".game-score");
+const roundScoreDisplay = document.querySelector(".round-score");
 const width = 8;
 const cards = [];
-let score = 0;
+let round = 1;
+let roundStarted = false;
+let roundScore = 0;
+let gameScore = 0;
+let roundGoals = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500];
 const suits = ["hearts", "diamonds", "spades", "clubs"];
 const faces = [
   "A",
@@ -58,7 +65,7 @@ function dragStart() {
   cardDraggedFace = this.innerText;
   cardIdDragged = parseInt(this.id);
   setTimeout(() => this.classList.add("invisible"), 0);
-  console.log(cardDraggedSuit, cardDraggedFace);
+  console.log(cardDraggedSuit, cardDraggedFace, this.style.backgroundImage);
 }
 
 function dragEnd() {
@@ -109,4 +116,379 @@ function dragDrop() {
   ].style.backgroundImage = `url(/images/${cardReplacedSuit}.png)`;
   cards[cardIdDragged].innerText = cardReplacedFace;
   cards[cardIdDragged].setAttribute("data-suit", cardReplacedSuit);
+  checkForStraightRow();
+  checkForStraightColumn();
+  checkColumnOfFive();
+  checkRowOfFive();
+  checkForFlushRow();
+  checkForFlushColumn();
+  checkRowOfFour();
+  checkColumnOfFour();
+  checkRowOfThree();
+  checkColumnOfThree();
+  topRowBugFix();
+  moveTilesDown();
+}
+
+// Match of 3 in a Row
+function checkRowOfThree() {
+  for (i = 0; i <= 61; i++) {
+    let rowOfThree = [i, i + 1, i + 2];
+    let faceToMatch = cards[i].innerText;
+    const isBlank = cards[i].style.backgroundImage === "";
+    const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
+    if (notValid.includes(i)) continue;
+    if (
+      rowOfThree.every((i) => cards[i].innerText === faceToMatch && !isBlank)
+    ) {
+      gameScoreUpdate(100);
+      rowOfThree.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      updateMarquee("Three Of A Kind!");
+      console.log("You got 3 in a row (row)");
+      return true;
+    }
+  }
+}
+
+checkRowOfThree();
+
+// Match of 3 in a Column
+function checkColumnOfThree() {
+  for (i = 0; i <= 47; i++) {
+    let columnOfThree = [i, i + width, i + width * 2];
+    let faceToMatch = cards[i].innerText;
+    const isBlank = cards[i].style.backgroundImage === "";
+    if (
+      columnOfThree.every((i) => cards[i].innerText === faceToMatch && !isBlank)
+    ) {
+      gameScoreUpdate(100);
+      columnOfThree.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got 3 in a row (col)");
+      updateMarquee("Three Of A Kind!");
+      return true;
+    }
+  }
+}
+checkColumnOfThree();
+
+// Match of 4 in a Row
+function checkRowOfFour() {
+  for (i = 0; i <= 60; i++) {
+    let rowOfFour = [i, i + 1, i + 2, i + 3];
+    let faceToMatch = cards[i].innerText;
+    const isBlank = cards[i].style.backgroundImage === "";
+    const notValid = [
+      5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+      54, 55,
+    ];
+    if (notValid.includes(i)) continue;
+    if (
+      rowOfFour.every((i) => cards[i].innerText === faceToMatch && !isBlank)
+    ) {
+      gameScoreUpdate(300);
+      rowOfFour.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got 4 in a row (row)");
+      updateMarquee("Four Of A Kind!");
+      return true;
+    }
+  }
+}
+
+// Match of 4 in a Column
+function checkColumnOfFour() {
+  for (i = 0; i < 39; i++) {
+    let columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+    let faceToMatch = cards[i].innerText;
+    const isBlank = cards[i].style.backgroundImage === "";
+    if (
+      columnOfFour.every((i) => cards[i].innerText === faceToMatch && !isBlank)
+    ) {
+      gameScoreUpdate(300);
+      columnOfFour.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("you got 4 in a row (col)");
+      updateMarquee("Four Of A Kind!");
+      return true;
+    }
+  }
+}
+
+// Match of 5 in a Row
+function checkRowOfFive() {
+  for (i = 0; i <= 59; i++) {
+    let rowOfFive = [i, i + 1, i + 2, i + 3, i + 4];
+    let faceToMatch = cards[i].innerText;
+    const isBlank = cards[i].style.backgroundImage === "";
+    const notValid = [
+      4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 36, 37, 38,
+      39, 44, 45, 46, 47, 52, 53, 54, 55,
+    ];
+    if (notValid.includes(i)) continue;
+    if (
+      rowOfFive.every((i) => cards[i].innerText === faceToMatch && !isBlank)
+    ) {
+      gameScoreUpdate(600);
+      rowOfFive.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got 5 in a row (row)");
+      updateMarquee("Five Of A Kind!");
+      return true;
+    }
+  }
+}
+
+// Match of 5 in a Column
+function checkColumnOfFive() {
+  for (i = 0; i < 31; i++) {
+    let columnOfFive = [
+      i,
+      i + width,
+      i + width * 2,
+      i + width * 3,
+      i + width * 4,
+    ];
+    let faceToMatch = cards[i].innerText;
+    const isBlank = cards[i].style.backgroundImage === "";
+    if (
+      columnOfFive.every((i) => cards[i].innerText === faceToMatch && !isBlank)
+    ) {
+      gameScoreUpdate(600);
+      columnOfFive.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got 5 in a row (col)");
+      updateMarquee("Five Of A Kind!");
+      return true;
+    }
+  }
+}
+
+// Check for a flush row
+function checkForFlushRow() {
+  for (i = 0; i <= 59; i++) {
+    let rowOfFive = [i, i + 1, i + 2, i + 3, i + 4];
+    let suitToMatch = cards[i].dataset.suit;
+    const isBlank = cards[i].style.backgroundImage === "";
+    const notValid = [
+      4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 36, 37, 38,
+      39, 44, 45, 46, 47, 52, 53, 54, 55,
+    ];
+    if (notValid.includes(i)) continue;
+    if (
+      rowOfFive.every((i) => cards[i].dataset.suit === suitToMatch && !isBlank)
+    ) {
+      gameScoreUpdate(50);
+      rowOfFive.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got a flush (row)");
+      updateMarquee("Flush!!");
+      return true;
+    }
+  }
+}
+
+// Check for Flush Column
+function checkForFlushColumn() {
+  for (i = 0; i <= 31; i++) {
+    let columnOfFive = [
+      i,
+      i + width,
+      i + width * 2,
+      i + width * 3,
+      i + width * 4,
+    ];
+    let suitToMatch = cards[i].dataset.suit;
+    const isBlank = cards[i].style.backgroundImage === "";
+    if (
+      columnOfFive.every(
+        (i) => cards[i].dataset.suit === suitToMatch && !isBlank
+      )
+    ) {
+      // update score
+      gameScoreUpdate(50);
+      columnOfFive.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got a flush (col)");
+      updateMarquee("Flush!!");
+      return true;
+    }
+  }
+}
+
+// check for a straight Row
+function checkForStraightRow() {
+  for (i = 0; i <= 59; i++) {
+    let rowOfFive = [i, i + 1, i + 2, i + 3, i + 4];
+    const setToMatch = rowOfFive.map((i) => cards[i].innerText).join("");
+    let faceToMatch = cards[i].innerText;
+    let suitToMatch = cards[i].dataset.suit;
+    const isBlank = cards[i].style.backgroundImage === "";
+    const notValid = [
+      4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31, 36, 37, 38,
+      39, 44, 45, 46, 47, 52, 53, 54, 55,
+    ];
+    if (notValid.includes(i)) continue;
+    if (isStraight(setToMatch) && !isBlank) {
+      // score += 50;
+      gameScoreUpdate(250);
+      rowOfFive.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got a straight (row)");
+      updateMarquee("Five Straight!");
+      return true;
+    }
+  }
+}
+
+// Check for Straight Column
+function checkForStraightColumn() {
+  for (i = 0; i < 31; i++) {
+    let columnOfFive = [
+      i,
+      i + width,
+      i + width * 2,
+      i + width * 3,
+      i + width * 4,
+    ];
+    const setToMatch = columnOfFive.map((i) => cards[i].innerText).join("");
+    let faceToMatch = cards[i].innerText;
+    const isBlank = cards[i].style.backgroundImage === "";
+    if (isStraight(setToMatch) && !isBlank) {
+      // update score
+      gameScoreUpdate(250);
+      columnOfFive.forEach((index) => {
+        cards[index].style.backgroundImage = "";
+        cards[index].innerText = "";
+        cards[index].setAttribute("data-suit", "");
+      });
+      console.log("You got a straight (col)");
+      updateMarquee("Five Straight!");
+      return true;
+    }
+  }
+}
+
+// Straight not in order true or false
+function isStraight(str) {
+  const sequence = "A2345678910JQKA";
+  let subString = [];
+
+  for (let i = 0; i <= sequence.length - str.length; i++) {
+    if (str.length === 5) subString = [i, i + 1, i + 2, i + 3, i + 4];
+    else subString = [i, i + 1, i + 2, i + 3, i + 4, i + 5];
+    let string1 = subString
+      .map((index) => sequence[index])
+      .sort()
+      .join("");
+
+    if (string1 === str.split("").sort().join("")) return true;
+  }
+  return false;
+}
+
+function topRowBugFix() {
+  const firstRowCards = [];
+  const restOfCards = [];
+  for (let i = 0; i <= 63; i++) {
+    if (i < 8) firstRowCards.push(cards[i]);
+    else restOfCards.push(cards[i]);
+  }
+  if (
+    firstRowCards.some((card) => card.innerText === "") &&
+    !restOfCards.some((card) => card.innerText === "")
+  ) {
+    firstRowCards.forEach((card) => {
+      if (card.innerText === "") {
+        let randomSuit = Math.floor(Math.random() * suits.length);
+        let randomFace = Math.floor(Math.random() * faces.length);
+        card.innerText = faces[randomFace];
+        card.style.backgroundImage = `url(/images/${suits[randomSuit]}.png)`;
+        card.setAttribute("data-suit", suits[randomSuit]);
+      }
+    });
+    return true;
+  }
+}
+
+// Tiles fall down
+function moveTilesDown() {
+  const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+  for (let i = 0; i <= 55; i++) {
+    if (
+      cards[i + width].style.backgroundImage === "" ||
+      cards[i + width].style.backgroundImage === `url("/images/.png")`
+    ) {
+      cards[i + width].style.backgroundImage = cards[i].style.backgroundImage;
+      cards[i + width].innerText = cards[i].innerText;
+      cards[i + width].setAttribute("data-suit", cards[i].dataset.suit);
+      cards[i].style.backgroundImage = "";
+      cards[i].innerText = "";
+      cards[i].setAttribute("data-suit", "");
+      const isFirstRow = firstRow.includes(i);
+      if (isFirstRow && cards[i].style.backgroundImage === "") {
+        let randomSuit = Math.floor(Math.random() * suits.length);
+        let randomFace = Math.floor(Math.random() * faces.length);
+        cards[i].innerText = faces[randomFace];
+        cards[
+          i
+        ].style.backgroundImage = `url(/images/${suits[randomSuit]}.png)`;
+        cards[i].setAttribute("data-suit", suits[randomSuit]);
+      }
+    }
+  }
+
+  if (cards.some((card) => card.style.backgroundImage === "")) moveTilesDown();
+  else return;
+}
+
+function updateMarquee(message) {
+  marqueeMessage.replaceChildren("");
+  const newMessage = `<li>${message}</li>`;
+  marqueeMessage.innerHTML = newMessage;
+}
+
+function gameScoreUpdate(points, roundStarted) {
+  // if (!roundStarted) return;
+  length = 9;
+  gameScore += points;
+  roundScore += points;
+  let gameScoreString = gameScore + "";
+  let roundScoreString = roundScore + "";
+  while (gameScoreString.length < length) {
+    gameScoreString = "0" + gameScoreString;
+  }
+  while (roundScoreString.length < length) {
+    roundScoreString = "0" + roundScoreString;
+  }
+  gameScoreDisplay.innerText = gameScoreString;
+  roundScoreDisplay.innerText = roundScoreString;
 }
